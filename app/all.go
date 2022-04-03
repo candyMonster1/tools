efebaf71fd79f638d8b322810cdf25333f4024c2
candy <414122451@qq.com>
2022-04-03 18:56:04 +0800

    add app

---
+++ app/all.go
@@ -0,0 +1,31 @@
+package app
+
+func InitAllApp() error {
+	// 先初始化内部app
+	for _, api := range internalApps {
+		if err := api.Config(); err != nil {
+			return nil
+		}
+	}
+
+	for _, api := range grpcApps {
+		if err := api.Config(); err != nil {
+			return err
+		}
+	}
+
+	for _, api := range httpApps {
+		if err := api.Config(); err != nil {
+			return err
+		}
+	}
+
+	for _, api := range ginApps {
+		if err := api.Config(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/candyMonster1/tools
+
+go 1.16
+
+require (
+	github.com/infraboard/mcube v1.7.3
+	github.com/pkg/errors v0.9.1
+	go.uber.org/zap v1.21.0
+)
